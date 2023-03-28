2ae6d512927a3223288b35b968c8d835fba4d622
southf09 <frankstonesouth@gmail.com>
2023-03-28 12:19:05 -0400

    Completed very basic react cards, styled with Bulma

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
@@ -5,12 +6,30 @@ import SiriImage from './images/siri.png';
 
 function App() {
     return (
-        <div>
-            <div>Personal Digital Assistants</div>
-
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
-            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage}/>
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage}/>
+    <div>
+        
+        <section class="hero is-primary">
+            <div class="hero-body">
+            <p class="title">
+            Personal Digital Assistants
+            </p>
+        </div>
+</section>   
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} description="Alexa is made by Amazon."/>
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} description="Cortana is made by Microsoft."/>   
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} description="Siri is made by Apple."/>   
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
     //Old
     // const title = props.title;
     // const handle = props.handle;
@@ -9,10 +9,22 @@ function ProfileCard({ title, handle, image }) {
     //Last change is changing the argument from props to the destructured title and handle
 
     return (
-        <div>
-            <img src={image} alt="pda logo" />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+        <div className="card-image">
+            <figure className="image is-1by1">
+                <img src={image} alt="pda logo" />
+            </figure>
+        </div>
+        <div className="card-content">
+            <div className="media-content">
+                <p className="title is-4">Title is {title}</p>
+                <p className="subtitle is-6">Handle is {handle}</p>
+            </div>
+            <div className="content">
+                {description}
+            </div>
+        </div>
+            
         </div>
         )
 }

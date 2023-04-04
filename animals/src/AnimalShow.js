bb6b4625ce74b3582e89283bac8b3b5773eef064
southf09 <frankstonesouth@gmail.com>
2023-04-04 10:16:27 -0400

    Added sample images, and imageMap that now renders the appropriate animal image, instead of writing the text

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,5 +1,23 @@
-function AnimalShow(){
-    return <div>animal</div>
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird: bird,
+    cat: cat,
+    cow: cow,
+    dog: dog,
+    gator: gator,
+    heart: heart,
+    horse: horse
+}
+
+function AnimalShow({ type }){
+    return <img alt="animal" src={svgMap[type]} />
 }
 
 export default AnimalShow;

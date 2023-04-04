2a34683c6081487eb4bcd0f3fd43828799b4a4f6
southf09 <frankstonesouth@gmail.com>
2023-04-04 14:27:06 -0400

    Added basic styling

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,3 +1,5 @@
+import './AnimalShow.css';
+import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
@@ -7,7 +9,7 @@ import heart from './svg/heart.svg';
 import horse from './svg/horse.svg';
 
 const svgMap = {
-    bird: bird,
+    bird,
     cat: cat,
     cow: cow,
     dog: dog,
@@ -17,7 +19,20 @@ const svgMap = {
 }
 
 function AnimalShow({ type }){
-    return <img alt="animal" src={svgMap[type]} />
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
+    return (
+    <div className="animal-show" onClick={handleClick}>
+        <img className="animal" alt="animal" src={svgMap[type]} />
+        <img className="heart" alt="heart" src={heart}
+        style={{ width: 10 + 10 * clicks + 'px' }}
+         />
+    </div>
+    );
 }
 
 export default AnimalShow;

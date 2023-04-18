dc69eb77c7b46bfc431e8627097ac80bb1d63f9f
southf09 <frankstonesouth@gmail.com>
2023-04-17 23:48:28 -0400

    Placeholder functions removed

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID 3dZHHrSg-rD2f8ZtmWj0vtzfl3t3SnIRX29gAtNG0KE'
         },
         params: {
-            query: 'hockey'
+            query: term
         }
     });
 
-    console.log(response);
-    return response;
+    return response.data.results;
 }
 
 export default searchImages;

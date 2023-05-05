21e3968ed91023798ca4f8d06191054f0b060510
southf09 <frankstonesouth@gmail.com>
2023-05-05 09:52:00 -0400

    Removed placeholder from onSubmit function, and added the dynamically updated term to be output in the console

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -7,7 +7,7 @@ function SearchBar({ onSubmit }) {
     const handleFormSubmit = (event) => {
         event.preventDefault();
 
-        onSubmit('cars');
+        onSubmit(term);
     }
 
     const handleChange = (event) => {
